d1764710a532f6fef142eb70ee82d7acfd013a6a
Yagmur Aydemir <rumaydemir02@gmail.com>
2025-12-14 16:20:58 +0100

    Initialize project with agence, commons, and hotel services
    Add initial Maven multi-module structure with 'agenceservice', 'commons', and 'hotelservice' modules. Each module includes basic Spring Boot setup, Maven wrapper scripts, and IDE configuration files.

---
+++ agenceservice/src/main/java/com/example/agenceservice/AgenceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.agenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceserviceApplication.class, args);
+	}
+
+}
---
+++ agenceservice/src/test/java/com/example/agenceservice/AgenceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
